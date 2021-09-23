0079e7768c950b428f2bb5d1d75ecb8df320c5ac
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-09-23 20:13:43 +0900

    Update alarmclock.java

--- alarmclock.java
+++ alarmclock.java
@@ -6,8 +6,9 @@ public class Main {
         while (true) {
             Scanner sc = new Scanner(System.in);
 
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            int a = sc.nextInt(); // 시간
+            int b = sc.nextInt(); // 분
+            int c, d, e; // 시간을 분으로 치환
             if ((a >= 0 && a <= 23) && (0 <= b && b <= 59)) {
                 if (b == 60) {
                     a++;
@@ -16,10 +17,21 @@ public class Main {
                     a++;
                     b = b - 60;
                 }
-                System.out.println("현재시간은 " + a + "시" + b + "분");
+                c = (a * 60) + b; // 총 분의 합
+                d = c / 60;
+                e = c % 60;
+                System.out.println("입력받은 값을 분으로 치환한 값은 " + c + " 입니다.");
+                System.out.println(c + " 를 시간으로 치환한 값은 " + d + " 입니다.");
+                System.out.println(c + " 를 분으로 치환한 값은 " + e + " 입니다.");
                 break;
             } else {
                 System.out.println("시간을 다시 입력해주세요..");
+            }
+
+            b = b - 45;
+            if (b < 0) {
+                a--;
+                b = b + 60;
             }
             sc.close();
         }
