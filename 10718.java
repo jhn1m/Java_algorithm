c3a0c054a154d4fad6cd9f1bfb629479fa003415
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-10-05 05:07:09 +0900

    Update 10718.java
    https://www.acmicpc.net/problem/10718

--- 10718.java
+++ 10718.java
@@ -1,8 +1,6 @@
-public class Main{
+public class Main{ 
   public static void main(Stirng args[]){
     System.out.println("First line");
     System.out.println("Second line");
   }
 }
-
-https://www.acmicpc.net/problem/10718
