a2f8ed813536fa3608de6be837c38293cb12c4b0
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-10-26 15:30:19 +0900

    Create 10950.java
    The number of test cases T is given in the first row.
    
    Each test case consists of a single line, and A and B are given to each line. (0 < A, B < 10)

---
+++ 10950.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first_Num, second_Num;
+        int test_CaseNum = sc.nextInt();
+        int sum_Array1[] = new int[test_CaseNum];
+        int sum_Array2[] = new int[test_CaseNum];
+
+        for (int i = 1; i <= test_CaseNum; i++) {
+            first_Num = sc.nextInt();
+            second_Num = sc.nextInt();
+            sum_Array1[i - 1] = first_Num;
+            sum_Array2[i - 1] = second_Num;
+        }
+
+        for (int i = 0; i < test_CaseNum; i++) {
+            System.out.println(sum_Array1[i] + sum_Array2[i]);
+        }
+
+        sc.close();
+    }
+}
