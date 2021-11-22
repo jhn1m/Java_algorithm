4e3f76a3e887203b541649c819242fb703c34362
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-11-23 00:39:18 +0900

    Update 3052.java

--- 3052.java
+++ 3052.java
@@ -1,7 +1,23 @@
-    private static void baekjoon() {
+/*
+problem
+When there are two natural numbers A and B, A%B is the remainder of A divided by B. For example, the remainder obtained by dividing 7, 14, 27, and 38 by 3 is 1, 2, 0, 2. 
+
+After receiving 10 inputs, the remainder divided by 42 is obtained. Then write a program that outputs how many different values there are.
+
+Input
+Numbers from the first line to the tenth line are given one per line. This number is less than or equal to 1,000 and is a nonnegative integer.
+
+Output
+In the first line, when divided by 42, output how many different remainings are.
+*/
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Main {
+
+    public static void main(String[] args) {
         int[] arr = new int[10];
-        int[] arr2 = new int[10];
-        int cnt = 10;
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < 10; i++) {
             int a = sc.nextInt();
@@ -9,13 +25,19 @@
                 arr[i] = a;
             }
         }
+        sc.close();
 
-        for (int i : arr) {
+        for (int i = 0; i < 10; i++) {
             arr[i] %= 42;
         }
         Arrays.sort(arr);
-        for (int i : arr) {
-            if (arr[i] == arr[i + 1]) {
+
+        int cnt2 = 0;
+        for (int i = 0; i < 9; i++) {
+            if (arr[i] != arr[i + 1]) {
+                cnt2++;
             }
         }
+        System.out.println(cnt2 + 1);
     }
+}
