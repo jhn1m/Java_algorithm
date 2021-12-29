0355b7d9339fb5c55b0c652ee451ccaaeab932b0
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-12-30 02:36:28 +0900

    Update 8958.java

--- 8958.java
+++ 8958.java
@@ -46,3 +46,37 @@ public class Main {
         }
     }
 }
+
+/*
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] Arr = new String[sc.nextInt()];
+        String[] Arr2;
+        sc.close();
+        for (int i = 0; i < Arr.length; i++) {
+            Arr[i] = sc.next();
+
+        }
+
+    }
+
+    void makeOX() {
+        // 각 문자열의 크기
+        int targetLength = (int) (Math.random() * 10);
+        Random rd = new Random();
+        // 아스키 코드를 이용하여 랜덤으로 문자추출
+        int O = 65;
+        int X = 66;
+        StringBuilder sb = new StringBuilder(targetLength);
+        for (int i = 0; i < targetLength; i++) {
+            int randomLimitedInt = O + (int) (rd.nextFloat() * (X - O + 1));
+            sb.append((char) randomLimitedInt);
+        }
+
+        // 추출된 문자를 치환하기
+        String generatedString = sb.toString();
+        generatedString = generatedString.replace("A", "O");
+        generatedString = generatedString.replace("B", "X");
+        System.out.println(generatedString);
+    }
+*/
