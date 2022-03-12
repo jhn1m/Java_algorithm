885c8094ef91f347e3a346753c43e5a502dbdeb1
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2022-03-12 14:23:16 +0900

    Update 18108.java

--- 18108.java
+++ 18108.java
@@ -1,20 +1,3 @@
-/*
-The question.
-Tim Redshift party, who had just arrived at Suvarnabhumi International Airport to participate in
-the ICPC Bangkok Regional, could not believe the snow. The airport's large screen said this year was 2562.
-
-Thailand, a Buddhist country, uses Buddhist prayer of immortality, that is,
-Buddhist prayer that counts the year based on the Nirvana year of Sakyamuni.
-On the other hand, Korea is using the year of the AD. Write a program that converts it
-into a standing year when given a blowing year.
-
-Input
-You will be given the year y of Bulgi, which you want to find out about the year of the Clerk. (1000 ≤ y ≤ 3000)
-
-Output
-Outputs the result of converting the blowing year to the standing year.
-*/
-
 import java.util.Scanner;
 
 public class Main {
