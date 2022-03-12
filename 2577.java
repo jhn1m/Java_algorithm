4c3db7b8a1e5aebe4314fc659bc90341d728b3a8
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2022-03-12 14:23:48 +0900

    Update 2577.java

--- 2577.java
+++ 2577.java
@@ -1,21 +1,3 @@
-/*
-problem
-Given three natural numbers A, B, and C, write a program
-to find out how many times each number from 0 to 9 was written in the result of calculating A × B × C.
-
-For example, if A = 150, B = 266, and C = 427, A × B × C = 150 × 266 × 427 = 17037300, and
-as a result of calculation, 0 was used three times, 1 was used one time, 3 was used two times, and 7 was used two times.
-
-Input
-A is given in the first line, B in the second line, and C in the third line.
-A, B, and C are all natural numbers greater than or equal to 100 and less than 1,000.
-
-Output
-The first line outputs how many times 0 has been written in the result of A × B × C. 
-Similarly, from the second line to the tenth line, we print out how many times the numbers
-from 1 to 9 were written in the results of A × B × C, one per line.
-*/
-
 import java.util.Arrays;
 import java.util.Scanner;
 
