cba4bb21aa87d334c75d85bc347a3cb2c7ad4759
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2023-01-04 13:52:51 +0900

    Create 25305.java

---
+++ 25305.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        System.out.println(arr[k - 1]);
+
+    }
+}
